03dfe2d528ddde020e7769243543b35c000babdf
nihardas09 <58418879+nihardas09@users.noreply.github.com>
2022-03-08 11:23:47 +0530

    Update main.go

--- hello-app/main.go
+++ hello-app/main.go
@@ -45,8 +45,8 @@ func main() {
 func hello(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Serving request: %s", r.URL.Path)
 	host, _ := os.Hostname()
-	fmt.Fprintf(w, "Hello, world!\n")
-	fmt.Fprintf(w, "Version: 1.0.0\n")
+	fmt.Fprintf(w, "Bye, world!\n")
+	fmt.Fprintf(w, "Version: 2.0\n")
 	fmt.Fprintf(w, "Hostname: %s\n", host)
 }
 
